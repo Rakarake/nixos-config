b65483ef8bedd1850840a5ae967b53c598e15e3c
Rakarake <rak@rakarake.xyz>
2023-08-06 13:56:58 +0200

    Removed a comment

--- desktop-configuration.nix
+++ desktop-configuration.nix
@@ -194,11 +194,6 @@
   # started in user sessions.
   # programs.mtr.enable = true;
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
