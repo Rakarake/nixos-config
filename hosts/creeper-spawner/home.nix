549023141e17a59e47d0a5218df35056979e2b85
Rakarake <rak@rakarake.xyz>
2023-09-20 17:00:39 +0200

    Yes

--- hosts/creeper-spawner/home.nix
+++ hosts/creeper-spawner/home.nix
@@ -4,6 +4,12 @@
   home.stateVersion = "23.05";
   programs.home-manager.enable = true;
 
+  # Direnv, used for bash nice
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+
   # Comfy shell
   imports = [ ../../home-shell.nix ];
 }
