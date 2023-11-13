90e0a0c5b67d07cdc68dffaf29fa3f4cc00377bf
Rakarake <rak@rakarake.xyz>
2023-11-13 23:43:15 +0100

    Changed podman to docker

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -168,18 +168,26 @@ in {
       #enableSSHSupport = true;
     };
 
-    # Podman
-    virtualisation = {
-      podman = {
+    # Docker
+    virtualisation.docker = {
+      enable = true;
+      rootless = {
         enable = true;
-
-        # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
-
-        # Required for containers under podman-compose to be able to talk to each other.
-        defaultNetwork.settings.dns_enabled = true;
+        setSocketVariable = true;
       };
     };
+    # Podman
+    #irtualisation = {
+    # podman = {
+    #   enable = true;
+
+    #   # Create a `docker` alias for podman, to use it as a drop-in replacement
+    #   dockerCompat = true;
+
+    #   # Required for containers under podman-compose to be able to talk to each other.
+    #   defaultNetwork.settings.dns_enabled = true;
+    # };
+    #;
 
     # Steam
     programs.steam = {
@@ -289,7 +297,7 @@ in {
     users.users.rakarake = {
       isNormalUser = true;
       description = "Rakarake";
-      extraGroups = [ "networkmanager" "wheel" "adbusers" ];
+      extraGroups = [ "networkmanager" "wheel" "adbusers" "docker" ];
     };
 
     # Mullvad Service
