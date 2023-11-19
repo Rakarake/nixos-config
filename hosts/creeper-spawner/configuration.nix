5da1f8d996b6d21f83f6ea9097268d6c06b0e0a0
Rakarake <rak@rakarake.xyz>
2023-11-19 23:06:20 +0100

    Fonts to server

--- hosts/creeper-spawner/configuration.nix
+++ hosts/creeper-spawner/configuration.nix
@@ -40,6 +40,21 @@ in
     btop
   ];
 
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    proggyfonts
+    nerdfonts
+    corefonts  # Microsoft fonts
+  ];
+
   # SSH daemon
   services.openssh = {
     enable = true;
