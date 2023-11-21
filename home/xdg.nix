d46d26b7e5415b74db9aac99c5a7c23a654e5c18
Rakarake <rak@rakarake.xyz>
2023-11-21 01:33:36 +0100

    Xdg changes

--- home/xdg.nix
+++ home/xdg.nix
@@ -6,41 +6,48 @@ let
   audioPlayer = ["mpv.desktop"];
   videoPlayer = ["mpv.desktop"];
   imageViewer = ["imv.desktop"];
+  mailClient  = ["thunderbird.desktop"];
+  pdfViewer   = ["org.gnome.Evince.desktop"];
+  fileManager = ["dolphin.desktop"];
 
   # XDG MIME types
   associations = {
     # Browser
-    "application/x-extension-htm" = browser;
-    "application/x-extension-html" = browser;
+    "application/x-extension-htm"   = browser;
+    "application/x-extension-html"  = browser;
     "application/x-extension-shtml" = browser;
-    "application/x-extension-xht" = browser;
+    "application/x-extension-xht"   = browser;
     "application/x-extension-xhtml" = browser;
-    "application/xhtml+xml" = browser;
-    "text/html" = browser;
-    "x-scheme-handler/about" = browser;
-    "x-scheme-handler/chrome" = ["chromium-browser.desktop"];
-    "x-scheme-handler/ftp" = browser;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/unknown" = browser;
+    "application/xhtml+xml"         = browser;
+    "text/html"                     = browser;
+    "x-scheme-handler/about"        = browser;
+    "x-scheme-handler/chrome"       = browser;
+    "x-scheme-handler/ftp"          = browser;
+    "x-scheme-handler/http"         = browser;
+    "x-scheme-handler/https"        = browser;
+    "x-scheme-handler/unknown"      = browser;
+
+    # Mail
+    "handler/mailto" = mailClient;
+
     # Multimedia
-    "audio/*"    = audioPlayer;
-    "audio/flac" = audioPlayer;
-    "audio/mp3"  = audioPlayer;
-    "video/*"    = videoPlayer;
-    "video/mp4"  = videoPlayer;
-    "video/mkv"  = videoPlayer;
-    "image/*"    = imageViewer;
-    "image/png"  = imageViewer;
-    "image/jpg"  = imageViewer;
-    "application/json" = browser;
-    "application/pdf" = ["org.gnome.Evince.desktop"];
+    "audio/*"         = audioPlayer;
+    "audio/flac"      = audioPlayer;
+    "audio/mp3"       = audioPlayer;
+    "video/*"         = videoPlayer;
+    "video/mp4"       = videoPlayer;
+    "video/mkv"       = videoPlayer;
+    "image/*"         = imageViewer;
+    "image/png"       = imageViewer;
+    "image/jpg"       = imageViewer;
+    "application/pdf" = pdfViewer;
+
     # File manager
-    "inode/directory" = ["dolphin.desktop"];
+    "inode/directory" = fileManager;
   };
 in {
   options.home-xdg = {
-    enable = mkEnableOption "Cozy home desktop (or laptop (or anything else)) config";
+    enable = mkEnableOption "Default apps";
   };
   config = mkIf cfg.enable {
     xdg = {
