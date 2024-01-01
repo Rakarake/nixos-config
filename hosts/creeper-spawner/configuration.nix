387f540240c5b73d5a3b9c044aed94f9e10085bf
Rakarake <rak@rakarake.xyz>
2024-01-01 21:18:29 +0100

    Minecraft again

--- hosts/creeper-spawner/configuration.nix
+++ hosts/creeper-spawner/configuration.nix
@@ -174,20 +174,20 @@ in
     useWizard = true;
   };
 
-  ## Minecraft server 1
-  #systemd.services.minecraft-server1 = {
-  #  enable = true;
-  #  path = [ pkgs.coreutils pkgs.jdk17 ];
-  #  wantedBy = [ "multi-user.target" ]; 
-  #  after = [ "network.target" ];
-  #  description = "Cool minecraft server";
-  #  serviceConfig = {
-  #    ExecStart = "${pkgs.tmux}/bin/tmux -S /var/minecraft-server1/tmux.socket new-session -s minecraft-server1-session -d /var/minecraft-server1/startserver.sh";
-  #    ExecStop = "${pkgs.tmux}/bin/tmux -S /var/minecraft-server1/tmux.socket kill-session -t minecraft-server1-session";
-  #    Type = "forking";
-  #    WorkingDirectory=/var/minecraft-server1;
-  #  };
-  #};
+  # Minecraft server 1
+  systemd.services.minecraft-server1 = {
+    enable = true;
+    path = [ pkgs.coreutils pkgs.jdk17 ];
+    wantedBy = [ "multi-user.target" ]; 
+    after = [ "network.target" ];
+    description = "Cool minecraft server";
+    serviceConfig = {
+      ExecStart = "${pkgs.tmux}/bin/tmux -S /var/minecraft-server1/tmux.socket new-session -s minecraft-server1-session -d /var/minecraft-server1/startserver.sh";
+      ExecStop = "${pkgs.tmux}/bin/tmux -S /var/minecraft-server1/tmux.socket kill-session -t minecraft-server1-session";
+      Type = "forking";
+      WorkingDirectory=/var/minecraft-server1;
+    };
+  };
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
