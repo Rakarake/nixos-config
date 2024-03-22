40a1f1b07d7cca0d30c063ee74565815e6ba27c4
Rakarake <rak@rakarake.xyz>
2024-03-22 12:34:32 +0100

    Creeper spawner SSD upgrade

--- hosts/creeper-spawner/configuration.nix
+++ hosts/creeper-spawner/configuration.nix
@@ -100,21 +100,47 @@ in
   # Hostname
   networking.hostName = "creeper-spawner";
 
-  # The data cluster
-  fileSystems."/data" = {
-    device = "/dev/disk/by-label/data";
-    fsType = "bcachefs";
+  ## The data cluster
+  #fileSystems."/data" = {
+  #  device = "/dev/sda4:/dev/nvme0n1";
+  #  fsType = "bcachefs";
+  #};
+  systemd.services.mount-data = {
+    description = "Bcachefs data mount";
+    script = "/run/current-system/sw/bin/mount -t bcachefs /dev/sda4:/dev/nvme0n1 /data";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    #serviceConfig = {
+    #  ExecStart = "/run/current-system/sw/bin/mount -t bcachefs /dev/sda4:/dev/nvme0n1 /data";
+    #  ExecStop = "/run/current-system/sw/bin/umount /var/lib/nextcloud";
+    #};
   };
-  # Nextcloud mount point
-  fileSystems."/var/lib/nextcloud" = {
-    device = "/data/nextcloud";
-    options = [ "bind" ];
+  systemd.services.mount-nextcloud = {
+    description = "Nextcloud data mount";
+    script = "/run/current-system/sw/bin/mount --bind /data/nextcloud /var/lib/nextcloud";
+    after = [ "mount-data.service" ];
+    wantedBy = [ "nextcloud-setup.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    #serviceConfig = {
+    #  ExecStart = "/run/current-system/sw/bin/mount --bind /data/nextcloud /var/lib/nextcloud";
+    #  ExecStop = "/run/current-system/sw/bin/umount /var/lib/nextcloud";
+    #};
   };
-  # Gitlab mount point
-  fileSystems."/var/gitlab" = {
-    device = "/data/gitlab";
-    options = [ "bind" ];
-  };
+
+  ## Nextcloud mount point
+  #fileSystems."/var/lib/nextcloud" = {
+  #  device = "/data/nextcloud";
+  #  options = [ "bind" ];
+  #};
+  ## Gitlab mount point
+  #fileSystems."/var/gitlab" = {
+  #  device = "/data/gitlab";
+  #  options = [ "bind" ];
+  #};
 
   # Open ports
   networking.firewall.allowedTCPPorts = lib.attrsets.attrValues ports;
@@ -230,22 +256,22 @@ in
     port = 8000;
   };
 
-  # Gitlab
-  services.gitlab = {
-    enable = true;
-    https = true;
-    # Https port when copying link for repo
-    port = 443;
-    host = hostnames.git;
-    initialRootPasswordFile = /data/secrets/rootPassword;
-    secrets = {
-      secretFile = /data/secrets/secret;
-      otpFile = /data/secrets/optsecret;
-      dbFile = /data/secrets/dbsecret;
-      jwsFile = pkgs.runCommand "oidcKeyBase" {} "${pkgs.openssl}/bin/openssl genrsa 2048 > $out";
-    };
-  };
-  systemd.services.gitlab-backup.environment.BACKUP = "dump";
+  ## Gitlab
+  #services.gitlab = {
+  #  enable = true;
+  #  https = true;
+  #  # Https port when copying link for repo
+  #  port = 443;
+  #  host = hostnames.git;
+  #  initialRootPasswordFile = /data/secrets/rootPassword;
+  #  secrets = {
+  #    secretFile = /data/secrets/secret;
+  #    otpFile = /data/secrets/optsecret;
+  #    dbFile = /data/secrets/dbsecret;
+  #    jwsFile = pkgs.runCommand "oidcKeyBase" {} "${pkgs.openssl}/bin/openssl genrsa 2048 > $out";
+  #  };
+  #};
+  #systemd.services.gitlab-backup.environment.BACKUP = "dump";
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
