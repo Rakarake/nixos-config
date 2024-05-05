f8f233986db617ade1f226e19e6ce0f25ae1144d
Rakarake <rak@rakarake.xyz>
2024-05-06 00:39:21 +0200

    simple-shell-utils, nix-goto

--- pkgs/simple-shell-utils/default.nix
+++ pkgs/simple-shell-utils/default.nix
@@ -1,30 +1,11 @@
-{ lib, pkgs }:
-let
-  utils = import ./utils.nix;
-  createBinSnippet = filename : content : ''printf "${content}" > $out/bin/${filename}'';
-in
-derivation {
+{ stdenv }:
+stdenv.mkDerivation {
   name = "simple-shell-utils";
-  system = pkgs.system;
-  builder = "${pkgs.bash}/bin/bash";
-  args = [
-    "-c"
-    ("${pkgs.coreutils-full}/bin/mkdir -p $out/bin\n" +
-    (lib.concatStringsSep "\n" (lib.attrValues (lib.mapAttrs createBinSnippet utils))) +
-    "\n${pkgs.coreutils-full}/bin/chmod -R +x $out/bin")
-  ];
+  src = ./.;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $(ls --ignore=default.nix) $out/bin
+    chmod -R +x $out/bin
+  '';
 }
-
-#{ lib, pkgs }:
-#let
-#  utils = import ./utils.nix;
-#  createBinSnippet = filename : content : ''printf "${content}" > $out/bin/${filename}'';
-#in
-#derivation {
-#  name = "simple-shell-utils";
-#  system = pkgs.system;
-#  builder = "${pkgs.bash}/bin/bash";
-#  args = [ "-c" "${pkgs.coreutils-full}/bin/mkdir -p $out/bin ; echo hello world > $out/bin/goobagoob" ];
-#}
-
 
