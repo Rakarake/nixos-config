0c31b6d578ce8c4f5d4240fb0cdca820a442ac7d
Rakarake <rak@rakarake.xyz>
2024-05-08 18:46:32 +0200

    Removed home manager hyprland flake dependency

--- home/global.nix
+++ home/global.nix
@@ -2,9 +2,6 @@
 { inputs, ... }: {
   imports = [
     inputs.nix-index-database.hmModules.nix-index
-    # For some reason, this cannot be imported in the hyprland module,
-    # I get infinate recursion.
-    inputs.hyprland.homeManagerModules.default
     # Enable to use development environment
     inputs.dev-stuff.homeManagerModules.default
   ];
