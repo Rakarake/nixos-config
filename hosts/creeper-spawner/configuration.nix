75db426a50feda7f7117c33f62c8f694b04ef20b
Rakarake <raka@rakarake.xyz>
2024-07-25 22:26:33 +0200

    Oh yeah

--- hosts/creeper-spawner/configuration.nix
+++ hosts/creeper-spawner/configuration.nix
@@ -238,16 +238,6 @@ in
   nixpkgs.config.packageOverrides = pkgs: {
     vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
   services.jellyfin = {
     enable = true;
   };
