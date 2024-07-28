779c23e8c75327291c3daf75e76f1ba39b618570
Rakarake <raka@rakarake.xyz>
2024-07-28 18:52:10 +0200

    Cozy home for steamed deck

---
+++ hosts/steamed-deck/home.nix
@@ -0,0 +1,10 @@
+{
+  imports = [ ../../home/rakarake ../../home/rakarake/theme.nix ];
+
+  # Wallpaper
+  #stylix.enable = true;
+  #stylix.image = ./wallpaper.png;
+
+  home-desktop.enable = true;
+  home-gnome.enable = true;
+}
