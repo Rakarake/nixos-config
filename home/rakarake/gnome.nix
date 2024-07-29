4429d59c3b42788599621ac77f93c49c9d400b7b
Rakarake <raka@rakarake.xyz>
2024-07-29 22:20:23 +0200

    Gnome nitpick

--- home/rakarake/gnome.nix
+++ home/rakarake/gnome.nix
@@ -36,7 +36,7 @@ in {
       };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>Return";
-        command = "kitty";
+        command = "kitty -e bash -c 'tmux attach || tmux'";
         name = "Terminal";
       };
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
