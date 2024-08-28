080ad4069d98008210aa7c2755de6e1e01848981
Magarnicle <51128122+TitaniusThorgor@users.noreply.github.com>
2024-08-28 13:36:51 +0200

    Update configuration.nix

--- hosts/mass-destruction/configuration.nix
+++ hosts/mass-destruction/configuration.nix
@@ -58,14 +58,6 @@ in
       fsType = "bcachefs";
     };
 
-hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    nvidiaSettings = true;
-  };
-
   # Open ports
   networking.firewall.allowedTCPPorts = lib.attrsets.attrValues ports;
   networking.firewall.allowedUDPPorts = lib.attrsets.attrValues ports;
