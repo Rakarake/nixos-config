da6ec228e91e9b8a3e0a1e005905dc89ec4d97af
Magarnicle <51128122+TitaniusThorgor@users.noreply.github.com>
2024-09-12 16:35:43 +0200

    Update configuration.nix

--- hosts/mass-destruction/configuration.nix
+++ hosts/mass-destruction/configuration.nix
@@ -109,6 +109,9 @@ in
     openttd
   ];
 
+      # Watch this not work
+      fastfetch = "${pkgs.fastfetch}/bin/fastfetch --logo ./nixos-config/hosts/mass-destruction/FastFetchMassDestructionArt";
+
   # SSH daemon
   services.openssh = {
     enable = true;
