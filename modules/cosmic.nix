768a6928d480853a6faddf0d00fc962e268a5c17
Rakarake <raka@rakarake.xyz>
2025-01-08 20:57:48 +0100

    Flake update, removed cosmic stuff that breaks, removed yuzu

--- modules/cosmic.nix
+++ modules/cosmic.nix
@@ -12,7 +12,7 @@ in {
       substituters = [ "https://cosmic.cachix.org/" ];
       trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
     };
-    services.desktopManager.cosmic.enable = true;
-    services.displayManager.cosmic-greeter.enable = true;
+    #services.desktopManager.cosmic.enable = true;
+    #services.displayManager.cosmic-greeter.enable = true;
   };
 }
