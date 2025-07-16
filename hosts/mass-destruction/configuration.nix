6ca5d53f843649b8fd75d8cafe2e16ffb4b6c23f
Magarnicle <51128122+TitaniusThorgor@users.noreply.github.com>
2025-07-16 21:48:54 +0200

    updated gooberjdkversion

--- hosts/mass-destruction/configuration.nix
+++ hosts/mass-destruction/configuration.nix
@@ -77,7 +77,7 @@ in
   imports = [
     ./hardware-configuration.nix
     (minecraftServerTemplate "minecraftserver-kreate" "A not so kreative minekraft server" pkgs.jdk17)
-    (minecraftServerTemplate "minecraftserver-goblainkraft" "Sin Bucket in Minecraft" pkgs.jdk17)
+    (minecraftServerTemplate "minecraftserver-goblainkraft" "Sin Bucket in Minecraft" pkgs.jdk21)
     (openttdServerTemplate "openTTDserver-massdestruction" "transporting destruction since 2024")
   ];
 
