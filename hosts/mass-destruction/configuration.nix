59fbc656eef9da4ff117be58553e5425dbba9644
Rakarake <raka@rakarake.xyz>
2025-07-19 14:16:26 +0200

    gregtacular

--- hosts/mass-destruction/configuration.nix
+++ hosts/mass-destruction/configuration.nix
@@ -78,6 +78,7 @@ in
     ./hardware-configuration.nix
     (minecraftServerTemplate "minecraftserver-kreate" "A not so kreative minekraft server" pkgs.jdk21)
     (minecraftServerTemplate "minecraftserver-goblainkraft" "Sin Bucket in Minecraft" pkgs.jdk21)
+    (minecraftServerTemplate "minecraftserver-terrafirmagreg" "michael" pkgs.jdk21)
     (openttdServerTemplate "openTTDserver-massdestruction" "transporting destruction since 2024")
   ];
 
