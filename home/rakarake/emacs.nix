b4f3c41f2f4286202233c3f1308cd1586e7911f7
Rakarake <raka@rakarake.xyz>
2025-09-15 12:35:14 +0200

    emacs copy paste

--- home/rakarake/emacs.nix
+++ home/rakarake/emacs.nix
@@ -9,6 +9,9 @@
       (load-file (let ((coding-system-for-read 'utf-8))
                  (shell-command-to-string "agda-mode locate"))
       )
+
+      ; Normal? copy-paste
+      (cua-mode t)
     '';
   };
 }
