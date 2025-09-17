bde698d62bef90a5a84b65a55029d521106b1a1d
Magarnicle <51128122+magarnicle27@users.noreply.github.com>
2025-09-17 11:34:10 +0200

    Update configuration.nix

--- hosts/mass-destruction/configuration.nix
+++ hosts/mass-destruction/configuration.nix
@@ -202,8 +202,8 @@ in
       serve_server_wellknown = true;
       registration_shared_secret_path = config.age.secrets.hotfreddy.path;
       auto_join_rooms = 
-          '#playwhen:chat.mdf.farm'
-          "#sunkencolumns:chat.mdf.farm" 
+         - #playwhen:chat.mdf.farm
+          #sunkencolumns:chat.mdf.farm
          - "#Reincarnated-as-a-Monke:chat.mdf.farm" 
          - "#NOMEMES:chat.mdf.farm" 
          - "#deadlocking:chat.mdf.farm" 
