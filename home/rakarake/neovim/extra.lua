7f8d858579ee41f948b2d524e97bdefcd5c4c316
Rakarake <raka@rakarake.xyz>
2025-12-20 16:01:13 +0100

    update and neovim

--- home/rakarake/neovim/extra.lua
+++ home/rakarake/neovim/extra.lua
@@ -40,6 +40,8 @@ vim.keymap.set('n', '<leader>fv', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope lsp_document_symbols<cr>')
+vim.keymap.set('n', '<leader>fw', '<cmd>Telescope lsp_dynamic_workspace_symbols<cr>')
 
 
 -- Buffers
