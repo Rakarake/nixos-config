08aea520cd532892196a1c3ea3b7963e8521f9d4
Rakarake <raka@rakarake.xyz>
2026-04-18 19:04:21 +0200

    rawa

--- home/rakarake/desktop.nix
+++ home/rakarake/desktop.nix
@@ -153,7 +153,7 @@ in
       appimage-run
       steamtinkerlaunch
       protonup-qt
-      baobab
+      kdePackages.filelight
       helvum
       nicotine-plus
       #ardour
@@ -203,6 +203,9 @@ in
       rssguard
       kdePackages.kdenlive
       pkgs-unstable.aichat
+      poppler-utils  # Needed for pdf RAG
+      pkgs-unstable.opencode
+      pkgs-unstable.yt-dlp
 
       # Emulators
       #fceux
@@ -250,40 +253,50 @@ in
     };
 
     # Rclone webdav nextcloud mount
-    programs.rclone = {
-      enable = true;
-      remotes."nextcloud" = {
-        secrets = {
-          pass = config.age.secrets.rakarake-rclone-webdav.path;
-        };
-        config = {
-          type = "webdav";
-          url = "https://nextcloud.rakarake.xyz/remote.php/dav/files/Rakarake";
-          vendor = "nextcloud";
-          user = "Rakarake";
+    #programs.rclone = {
+    #  enable = true;
+    #  remotes."nextcloud" = {
+    #    secrets = {
+    #      pass = config.age.secrets.rakarake-rclone-webdav.path;
+    #    };
+    #    config = {
+    #      type = "webdav";
+    #      url = "https://nextcloud.rakarake.xyz/remote.php/dav/files/Rakarake";
+    #      vendor = "nextcloud";
+    #      user = "Rakarake";
 
-        };
-        mounts = {
-          # The root
-          "/" = {
-            enable = true;
-            mountPoint = "/home/${user}/Nextcloud";
-            options = {
-              vfs-cache-mode = "writes";
-              allow-other = true;
+    #    };
+    #    mounts = {
+    #      # The root
+    #      "/" = {
+    #        enable = true;
+    #        mountPoint = "/home/${user}/Nextcloud";
+    #        options = {
+    #          vfs-cache-mode = "full";
+    #          vfs-cache-max-age = "1h";
+    #          vfs-cache-max-size = "1G";
+    #          buffer-size = "16M";
+    #          dir-cache-time = "72h";
+    #          umask = "002";
+    #          uid = 1000;
+    #          gid = 1000;
+    #          daemon-timeout = 3;
 
-              # Options to reduce excessive calls?
-              tpslimit = 2;
-              tpslimit-burst = 2;
-              checkers = 4;
-              transfers = 2;
-              dir-cache-time = "12h";
-              poll-interval = 0;
-            };
-          };
-        };
-      };
-    };
+    #          #vfs-cache-mode = "writes";
+    #          #allow-other = true;
+
+    #          ## Options to reduce excessive calls?
+    #          #tpslimit = 2;
+    #          #tpslimit-burst = 2;
+    #          #checkers = 4;
+    #          #transfers = 2;
+    #          #dir-cache-time = "12h";
+    #          #poll-interval = 0;
+    #        };
+    #      };
+    #    };
+    #  };
+    #};
 
     # Nextcloud webdav rclone mount
     #systemd.user.mounts."home-${user}-Nextcloud" = {
