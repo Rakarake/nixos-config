a3118cde24aec42baf431c57b6b6b83f2f51574f
Magarnicle <scolipede2@hotmail.com>
2026-05-16 12:04:55 +0200

    suss

--- hosts/mass-destruction/matrix.nix
+++ hosts/mass-destruction/matrix.nix
@@ -38,6 +38,8 @@ in
       LC_CTYPE = "C";
   '';
 
+  networking.firewall.allowedUDPPortRanges = [ { from = 49000; to = 50000; } ];
+
   services.matrix-synapse = {
     enable = true;
     settings = {
